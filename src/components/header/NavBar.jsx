8d7e93bd985b909c24c4bbca1d302d6b4a9842f1
Ambrose <ambrosetall@gmail.com>
2023-08-29 18:20:45 +0300

    Changes

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -4,6 +4,9 @@ import React from "react";
 import styles from "./page.module.css";
 import Darkmode from "../darkMode/page";
 
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
 const navlinks = [
   { id: 1, name: "Home", url: "/" },
   { id: 2, name: "Portfolio", url: "/portfolio" },
@@ -11,27 +14,36 @@ const navlinks = [
   { id: 5, name: "Contact", url: "/contact" },
 ];
 
-const NavBar = () => {
-  // const [isMenuOpen, setIsMenuOpen] = useState(false);
+function Navbar() {
+  const navRef = useRef();
 
-  // const toggleMenu = () => {
-  //   setIsMenuOpen(!isMenuOpen);
-  // };
+  const showNavbar = () => {
+    navRef.current.classList.toggle(styles.responsive_nav);
+  };
+
   return (
     <div className={styles.container}>
       <Link className={styles.logo} href="/">
         Ambrose
       </Link>
-      <div className={styles.links}>
+      <div ref={navRef} className={styles.links}>
         <Darkmode />
         {navlinks.map((link) => (
           <Link className={styles.navlinks} key={link.id} href={link.url}>
             {link.name}
           </Link>
         ))}
+
+        <button className={styles.navBtnCls} onClick={showNavbar}>
+          <FaTimes />
+        </button>
       </div>
+
+      <button className={styles.navBtn} onClick={showNavbar}>
+        <FaBars />
+      </button>
     </div>
   );
-};
+}
 
-export default NavBar;
+export default Navbar;
